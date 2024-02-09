10c8e488d7ee91c55b50b2c1fbfe794248a6d209
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-02-09 17:25:03 +0530

    update redirect service

--- redirect_service/go.mod
+++ redirect_service/go.mod
@@ -4,19 +4,20 @@ go 1.20
 
 require (
 	github.com/gorilla/mux v1.8.1
-	github.com/ivinayakg/shorte.live/api v0.0.0-20240131204828-141063de6c82
+	github.com/ivinayakg/shorte.live/api v0.0.0-20240209115128-24679d4dbe8a
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.13.1
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/avct/uasurfer v0.0.0-20191028135549-26b5daa857f1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.5 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/redis/go-redis/v9 v9.4.0 // indirect
 	github.com/rs/cors v1.10.1 // indirect
@@ -24,7 +25,7 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
-	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
--- redirect_service/main.go
+++ redirect_service/main.go
@@ -5,16 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/ivinayakg/shorte.live/api/controllers"
 	"github.com/ivinayakg/shorte.live/api/helpers"
 	"github.com/ivinayakg/shorte.live/api/middleware"
-	"github.com/ivinayakg/shorte.live/api/models"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var UI_URL string
@@ -24,75 +20,6 @@ func RedirectHome(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, UI_URL, http.StatusSeeOther)
 }
 
-func ResolveURL(w http.ResponseWriter, r *http.Request) {
-	url := &models.URL{}
-	urlExpiredOrNotFound := true
-	var err error
-
-	systemNotAvailable := helpers.SystemUnderMaintenance()
-	if systemNotAvailable {
-		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-		http.Redirect(w, r, os.Getenv("FRONTEND_URL_MAINTENANCE"), http.StatusMovedPermanently)
-		return
-	}
-
-	defaultLimit, found := helpers.GetRateConfig(false).Limit["dynamic"]
-	if !found {
-		defaultLimit = &helpers.URLLimit{Value: 100, Expiry: 30}
-	}
-	info, err := helpers.RateLimit(r, "", defaultLimit)
-	if err != nil {
-		helpers.SendJSONError(w, http.StatusTooManyRequests, fmt.Errorf("you have exhausted your quota for %v, %v to retry again", "Resolve URL", helpers.TimeRemaining(info)).Error())
-		return
-	}
-
-	vars := mux.Vars(r)
-	urlShort := vars["short"]
-	currentTime := time.Now()
-
-	revalidateCache, err := strconv.ParseBool(r.URL.Query().Get("revalidate"))
-	if err != nil {
-		fmt.Println("Error:", err)
-		revalidateCache = false
-	}
-
-	err = helpers.Redis.GetJSON(urlShort, url)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if url.ID != primitive.NilObjectID && !revalidateCache {
-		if !currentTime.After(time.Unix(int64(url.Expiry), 0)) {
-			urlExpiredOrNotFound = false
-		}
-	} else {
-		url, err = models.GetURL(urlShort, "")
-		if err != nil && err != mongo.ErrNoDocuments {
-			helpers.SendJSONError(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		if err != mongo.ErrNoDocuments && !currentTime.After(time.Unix(int64(url.Expiry), 0)) {
-			urlExpiredOrNotFound = false
-			go helpers.Redis.SetJSON(urlShort, url, time.Until(time.Unix(int64(url.Expiry), 0)))
-		}
-	}
-
-	if urlExpiredOrNotFound || url == nil {
-		notFoundUrl := os.Getenv("UI_NOT_FOUND_URL")
-		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-		http.Redirect(w, r, notFoundUrl, http.StatusTemporaryRedirect)
-		return
-	}
-
-	// go func(urlId string) {
-	// 	currTime := time.Now()
-	// 	models.UpdateUserURLVisited(urlId, currTime)
-	// }(url.ID.Hex())
-
-	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-	http.Redirect(w, r, url.Destination, http.StatusMovedPermanently)
-}
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -109,7 +36,7 @@ func main() {
 	r.Use(middleware.LogMW)
 
 	r.HandleFunc("/", RedirectHome).Methods("GET", "POST", "PUT", "PATCH", "DELETE")
-	r.HandleFunc("/{short}", ResolveURL).Methods("GET")
+	r.HandleFunc("/{short}", controllers.ResolveURL).Methods("GET")
 
 	fmt.Println("Starting the server on port " + PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), r))
