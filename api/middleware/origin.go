6dbbe39c5bb59052880026f8e4004da5261dbf88
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-03-01 22:11:33 +0530

    logging in origin handler

--- api/middleware/origin.go
+++ api/middleware/origin.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"regexp"
@@ -12,6 +13,7 @@ func OriginHandler(next http.Handler) http.Handler {
 	var RedirectServiceUrl = os.Getenv("SHORTED_URL_DOMAIN")
 	notFoundUrl := os.Getenv("UI_NOT_FOUND_URL")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Host, RedirectServiceUrl)
 		if r.Host == RedirectServiceUrl && !re.MatchString(r.URL.Path) {
 			w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 			http.Redirect(w, r, notFoundUrl, http.StatusTemporaryRedirect)
